58db6b2494b15bc712145f44e1b917e244044d61
Nishant_Satish08 <86757803+Nishantnix08@users.noreply.github.com>
2021-10-29 11:29:24 +0530

    Update MorningGreeting.java

--- MorningGreeting.java
+++ MorningGreeting.java
@@ -1,9 +1,29 @@
 package com.annotations.example3;
 
-public class MorningGreeting {
+import org.springframework.stereotype.Component;
+
+@Component
+public class MorningGreeting implements Greeting {
+	
+	public String greeting = "good moring";
 
 	public MorningGreeting() {
+		super();
 		// TODO Auto-generated constructor stub
+		System.out.println(greeting);
+	}
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+
+	public String greet() {
+		// TODO Auto-generated method stub
+		return greeting;
 	}
 
 }
